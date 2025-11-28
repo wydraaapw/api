b00b16e5f474c5198d7ce699f40effb363bc3c7e
Paweł Wydrych <pawel.wydrych2002@gmail.com>
2025-11-28 17:58:46 +0100

    feat(model): implement Client entity

---
+++ src/main/java/org/example/api/model/Client.java
@@ -0,0 +1,39 @@
+package org.example.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "clients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Client {
+    @Id
+    private Long id;
+
+    @Column(length = 500)
+    private String aboutMe;
+
+    @Column(length = 500)
+    private String allergies;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    @MapsId
+    private User user;
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Client client &&
+                Objects.equals(client.getId(), this.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
