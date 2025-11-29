cfb269210972a7497ef379c180a4dd4edbc37387
Paweł Wydrych <pawel.wydrych2002@gmail.com>
2025-11-29 21:43:03 +0100

    feat(model): implement RestaurantTable entity

---
+++ src/main/java/org/example/api/model/RestaurantTable.java
@@ -0,0 +1,41 @@
+package org.example.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "restaurant_tables")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class RestaurantTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Integer tableNumber;
+
+    @Column(nullable = false)
+    private Integer seats;
+
+    @Column(nullable = false)
+    private Integer rowPosition;
+
+    @Column(nullable = false)
+    private Integer columnPosition;
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof RestaurantTable restaurantTable &&
+                this.id != null &&
+                this.id.equals(restaurantTable.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
